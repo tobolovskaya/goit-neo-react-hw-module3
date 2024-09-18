a3d19af2b2a768f56ab983d1461320cdb2d2474a
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-09-18 08:50:54 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,20 +6,17 @@ import SearchBox from '../SearchBox/SearchBox';
 import styles from './App.module.css';
 
 const App = () => {
-  // Ініціалізуємо стан контактів, зчитуючи їх з localStorage, або за замовчуванням використовуємо порожній масив
   const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem('contacts');
     return savedContacts ? JSON.parse(savedContacts) : [];
   });
 
   const [filter, setFilter] = useState('');
-
-  // Використовуємо useEffect для збереження контактів у localStorage при кожній зміні контактів
+  
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  // Додавання нового контакту
   const addContact = (newContact) => {
     const isDuplicate = contacts.some(
       (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
@@ -33,12 +30,10 @@ const App = () => {
     setContacts((prevContacts) => [...prevContacts, { id: nanoid(), ...newContact }]);
   };
 
-  // Видалення контакту
   const deleteContact = (contactId) => {
     setContacts((prevContacts) => prevContacts.filter(contact => contact.id !== contactId));
   };
 
-  // Фільтрація контактів
   const getFilteredContacts = () => {
     return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
@@ -49,13 +44,10 @@ const App = () => {
     <div className={styles.container}>
       <h1>Phonebook</h1>
       
-      {/* Форма для додавання контакту */}
       <ContactForm addContact={addContact} />
 
-      {/* Поле пошуку */}
       <SearchBox filter={filter} setFilter={setFilter} />
 
-      {/* Список контактів */}
       <ContactList contacts={getFilteredContacts()} deleteContact={deleteContact} />
     </div>
   );
